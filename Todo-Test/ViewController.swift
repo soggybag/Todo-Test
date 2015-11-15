aa9bc7f2df415982167df40cd4ae090f2c0b5fe0
Mitchell Hudson <soggybag@gmail.com>
2015-11-15 10:32:21 -0800

    Initial Commit

---
+++ Todo-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo-Test
+//
+//  Created by mitchell hudson on 11/15/15.
+//  Copyright © 2015 mitchell hudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
